ba7d1bec09cb67b3a257fce08f8e3d61c6ba8725
chibiegg <chibiegg@chibiegg.net>
2015-11-10 16:34:01 +0900

    共通関数をcommon.phpに分離

--- public/student/api.php
+++ public/student/api.php
@@ -1,71 +1,6 @@
 <?php
-require_once("Mail.php");
 include_once(dirname(__FILE__) . "/../../config.php");
-
-function response($response_code, $data) {
-	http_response_code($response_code);
-	header('Content-Type: application/json');
-	$body = json_encode($data);
-	echo $body;
-}
-
-function sendmail($to, $coupon){
-	// 言語と文字エンコーディングを正しくセット
-	mb_language("Japanese");
-	mb_internal_encoding("UTF-8");
-
-	$params = array(
-	  "host" => ini_get("SMTP"),   // SMTPサーバー名
-	  "port" => 25,              // ポート番号
-	  "auth" => false,            // SMTP認証を使用する
-	);
-
-// メール
-	$message = "
-こんにちは、
-
-CROSS 実行委員会　です。
-ユーザー情報をご登録頂きありがとうございます。
-
-学割クーポンを発行いたしましたので、
-チケット購入手続きの際に、ご入力ください。
-
-クーポンコード       : $coupon
-チケット販売サイトURL : https://peatix.com/sales/event/125846/tickets
-
-
-その他、ご要望、ご質問などは、
-CROSS公式Facebookページ、Twitterアカウントまでお問い合わせください。
-
-facebook: https://www.facebook.com/engineersupportCROSS
-Twitter : https://twitter.com/e_s_cross"
-;
-
-	$subject = '学割クーポン発行のお知らせ';
-
-	$headers = array(
-	  "To" => $to,
-	  "From" => "invitation@cross-party.com",
-	  "Subject" => mb_encode_mimeheader($subject)
-	);
-
-	$message = mb_convert_encoding($message, "ISO-2022-JP", "auto");
-	$mailObject = Mail::factory("smtp", $params);
-	return $mailObject->send($to, $headers, $message);
-}
-
-if ($_SERVER["REQUEST_METHOD"] !== "POST") {
-	response(405, array("result"=>"error", "message"=>"method not allowed"));
-	exit(0);
-}
-
-$json_string = file_get_contents('php://input');
-$data = json_decode($json_string, TRUE);
-
-if ($data === NULL) {
-	response(422, array("result"=>"error", "message"=>"invalid format"));
-	exit(0);
-}
+include_once(dirname(__FILE__) . "/../../common.php");
 
 try{
 	$pdo = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
@@ -112,8 +47,31 @@ try{
 	$stmt->execute();
 	$code = $stmt->fetchColumn();
 
+	// メール本文
+	$message = "こんにちは、
+
+CROSS 実行委員会　です。
+ユーザー情報をご登録頂きありがとうございます。
+
+学割クーポンを発行いたしましたので、
+チケット購入手続きの際に、ご入力ください。
+
+クーポンコード       : $coupon
+チケット販売サイトURL : https://peatix.com/sales/event/125846/tickets
+
+
+その他、ご要望、ご質問などは、
+CROSS公式Facebookページ、Twitterアカウントまでお問い合わせください。
+
+facebook: https://www.facebook.com/engineersupportCROSS
+Twitter : https://twitter.com/e_s_cross"
+;
+
+	$subject = '学割クーポン発行のお知らせ';
+
+
 	// メール送信
-	if (!sendmail($data['mail'], $code)){
+	if (!sendmail($data['mail'], $subject, $message)){
 
 	};
 
